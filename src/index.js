b0d7d94fb5228c87b3ba9be9aba40eb587563196
Ramana Sree K V <ramanarobotech@gmail.com>
2025-02-14 23:35:07 +0530

    update index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
+import App from "./App";
 import { auth0Config } from "./auth0-config";
 
-const root = ReactDOM.createRoot(
-<Auth0Provider 
-    domain={auth0Config.domain} 
-    clientId={auth0Config.clientId}
-    authorizationParams={auth0Config.authorizationParams}
->
-    <App />
-</Auth0Provider>,
+// Ensure the root element exists before rendering
+const rootElement = document.getElementById("root");
 
-document.getElementById("root"));
-root.render(<App />);
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <Auth0Provider 
+      domain={auth0Config.domain} 
+      clientId={auth0Config.clientId}
+      authorizationParams={auth0Config.authorizationParams}
+    >
+      <App />
+    </Auth0Provider>
+  );
+} else {
+  console.error("Root element not found! Check index.html for an element with id='root'.");
+}
