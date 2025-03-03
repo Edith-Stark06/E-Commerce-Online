59404d6209792654619702386ecadd125a8b5b17
Ramana Sree K V <ramanarobotech@gmail.com>
2025-03-04 00:49:46 +0530

    img commit

--- src/App.js
+++ src/App.js
@@ -72,12 +72,20 @@ function App() {
   // --------------- LOGIN PAGE ---------------
   if (!isAuthenticated) {
     return (
-      <div style={loginPageStyle}>
-        <h1>Welcome to E-Commerce Shoe Website</h1>
-        <p>Please login to continue</p>
-        <button onClick={() => loginWithRedirect()} style={buttonStyle}>
-          Login / Signup
-        </button>
+      <div style={loginPageContainer}>
+        <div style={loginCard}>
+        <img
+          src="https://i.pinimg.com/736x/c3/b3/14/c3b3146e35033a66d563dbce4e53a0b7.jpg"
+          alt="Logo"
+          style={logoStyle}
+        />
+          <h2 style={loginTitle}>WELCOME    </h2>
+          <p style={loginSubtitle}>Sign in to your account</p>
+
+          <button onClick={() => loginWithRedirect()} style={loginButton}>
+            Login
+          </button>
+        </div>
       </div>
     );
   }
@@ -103,12 +111,55 @@ function App() {
 }
 
 // Styles
-const loginPageStyle = {
-  textAlign: "center",
-  marginTop: "100px",
+const loginPageContainer = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "100vh",
+  backgroundImage: "url('https://static.vecteezy.com/system/resources/previews/002/006/967/non_2x/young-women-takes-a-shopping-cart-and-enjoy-online-shopping-through-smartphones-choose-to-buy-gifts-valentine-s-day-concepts-website-or-mobile-phone-application-flat-design-illustration-vector.jpg')",
+  backgroundSize: "100% 100%",
+  backgroundPosition: "center",
+  position: "relative",
+  
 };
 
-const buttonStyle = {
+const overlay = {
+  position: "absolute",
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "100%",
+  backgroundColor: "rgba(0, 0, 0, 0.5)", // Dark overlay for better contrast
+};
+
+const loginCard = {
+  width: "400px",
+  padding: "30px",
+  backgroundColor: "white",
+  borderRadius: "10px",
+  boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+  textAlign: "center",
+};
+
+const logoStyle = {
+  width: "350px",
+  height: "250px",
+};
+
+const loginTitle = {
+  fontSize: "24px",
+  fontWeight: "bold",
+  marginBottom: "10px",
+  color: "#333",
+};
+
+const loginSubtitle = {
+  fontSize: "14px",
+  color: "#777",
+  marginBottom: "20px",
+};
+
+const loginButton = {
   padding: "10px 20px",
   fontSize: "16px",
   cursor: "pointer",
@@ -116,6 +167,19 @@ const buttonStyle = {
   color: "white",
   border: "none",
   borderRadius: "5px",
+  width: "100%",
+  marginBottom: "10px",
+};
+
+const signupText = {
+  fontSize: "14px",
+  color: "#555",
+};
+
+const signupLink = {
+  color: "#007bff",
+  textDecoration: "none",
+  fontWeight: "bold",
 };
 
 const logoutButtonStyle = {
