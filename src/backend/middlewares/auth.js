bf34bf6b137eae2fa61b4ff50bd7dd9410ccff54
Ramana Sree K V <prasanth.v2023@vitstudent.ac.in>
2025-04-17 20:31:16 +0530

    checkpoint1

--- src/backend/middlewares/auth.js
+++
@@ -1,9 +0,0 @@
-const { auth } = require('express-oauth2-jwt-bearer');
-
-const authMiddleware = auth({
-    audience: '5MJhiaQrfGuFPoWKWzdamByF48q2JdpT',
-    issuerBaseURL: 'https://dev-8f4008hilw3qd6ul.us.auth0.com',
-    tokenSigningAlg: 'RS256'
-});
-
-module.exports = authMiddleware; 
